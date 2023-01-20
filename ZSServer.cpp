1abd57b4a069629948da77364b5dd29fc77d0e9b
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2023-01-20 08:59:46 -0500

    ok

--- ZSServer.cpp
+++ ZSServer.cpp
@@ -6,16 +6,18 @@ ZSServer::ZSServer(ZwiftShaper *zs, BLEServer *ble_server, BLEClient *ble_client
   server = ble_server ;
   client = ble_client ;
   client_connected = false ;
+
+  server->setCallbacks(this) ;
+  BLEAdvertising *adv = BLEDevice::getAdvertising() ;
+  adv->setScanResponse(true) ;
+  adv->setMinPreferred(0x06) ;
 }
 
 
 void ZSServer::startAdvertizing(){
   std::string name = std::string("ZS[") + shaper->getName() + std::string("]") ;
   ::esp_ble_gap_set_device_name(name.c_str()) ;
-  server->setCallbacks(this) ;
-  BLEAdvertising *adv = BLEDevice::getAdvertising() ;
-  adv->setScanResponse(true) ;
-  adv->setMinPreferred(0x06) ;
+
   BLEDevice::startAdvertising() ;
 }
 
@@ -31,7 +33,7 @@ void ZSServer::onConnect(BLEServer *srv) {
 void ZSServer::onDisconnect(BLEServer *srv) {
   Serial.println("Disconnected") ;
   client_connected = false ;
-  // BLEDevice::startAdvertising() ;
+  // startAdvertizing() ;
 }
 
 
@@ -39,15 +41,10 @@ void ZSServer::onRead(BLECharacteristic *chr){
 }
 
 
-void ZSServer::onWrite (BLECharacteristic *chr){
+void ZSServer::onWrite(BLECharacteristic *chr){
 }
 
 
 bool ZSServer::isClientConnected(){
   return client_connected ;
-}
-
-
-BLEServer *ZSServer::getServer(){
-  return server ;
 }
